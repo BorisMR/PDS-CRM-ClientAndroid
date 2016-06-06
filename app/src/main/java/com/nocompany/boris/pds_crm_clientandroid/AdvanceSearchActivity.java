230e3793e562fe0f50d46893d1b53d54b2622e52
BorisMR <anum.bmr.01@gmail.com>
2016-06-05 22:09:40 -0400

    Main menu created, simple search in development

---
+++ app/src/main/java/com/nocompany/boris/pds_crm_clientandroid/AdvanceSearchActivity.java
@@ -0,0 +1,13 @@
+package com.nocompany.boris.pds_crm_clientandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AdvanceSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_advance_search);
+    }
+}
--- app/src/main/java/com/nocompany/boris/pds_crm_clientandroid/MainActivity.java
+++ app/src/main/java/com/nocompany/boris/pds_crm_clientandroid/MainActivity.java
@@ -1,5 +1,6 @@
 package com.nocompany.boris.pds_crm_clientandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +20,26 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        Button btnMainMenuSimple = (Button) findViewById(R.id.btnMainMenuSimple);{
+            btnMainMenuSimple.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View view){
+                    Intent activitySimpleSearch = new Intent(MainActivity.this, SimpleSearchActivity.class);
+                    startActivity(activitySimpleSearch);
+                };
+            });
+        }
+
+        Button btnMainMenuAdvance = (Button) findViewById(R.id.btnMainMenuAdvance);{
+            btnMainMenuAdvance.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View view){
+                    Intent activityAdvanceSearch = new Intent(MainActivity.this, AdvanceSearchActivity.class);
+                    startActivity(activityAdvanceSearch);
+                };
+            });
+        }
+    /*
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
     }
 
     @Override
